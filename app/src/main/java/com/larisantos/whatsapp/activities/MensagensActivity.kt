abb92af084df821e4c5ad849eb57f6e2312ce863
Larissa Santos <larynha_santos@yahoo.com.br>
2023-12-27 19:14:25 -0300

    Primeiro Commit

--- app/src/main/java/com/larisantos/whatsapp/activities/MensagensActivity.kt
+++ app/src/main/java/com/larisantos/whatsapp/activities/MensagensActivity.kt
@@ -3,11 +3,15 @@ package com.larisantos.whatsapp.activities
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.larisantos.whatsapp.R
-import com.larisantos.whatsapp.databinding.ActivityCadastroBinding
+import com.google.firebase.firestore.ListenerRegistration
+import com.google.firebase.firestore.Query
+import com.larisantos.whatsapp.adapters.ConversasAdapter
 import com.larisantos.whatsapp.databinding.ActivityMensagensBinding
+import com.larisantos.whatsapp.model.Conversa
 import com.larisantos.whatsapp.model.Mensagem
 import com.larisantos.whatsapp.model.Usuario
 import com.larisantos.whatsapp.utils.Constantes
@@ -28,14 +32,72 @@ class MensagensActivity : AppCompatActivity() {
         ActivityMensagensBinding.inflate(layoutInflater)
     }
 
+    private lateinit var listenerRegistration: ListenerRegistration
+
     private var dadosDestinatario: Usuario? = null
+    private var dadosUsuarioRemetente: Usuario? = null
+    private lateinit var conversasAdapter: ConversasAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        recuperarDadosUsuarioDestinatario()
+        recuperarDadosUsuarios()
         inicializarToobar()
         inicializarEventoClique()
+        inicializarRecyclerview()
+        inicializarListeners()
+    }
+
+    private fun inicializarRecyclerview() {
+
+        with(binding){
+            conversasAdapter = ConversasAdapter()
+            rvMensagens.adapter = ConversasAdapter()
+            rvMensagens.layoutManager = LinearLayoutManager(applicationContext)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        listenerRegistration.remove()
+    }
+
+    private fun inicializarListeners() {
+        val idUsuarioRemetente = firebaseAuth.currentUser?.uid
+        val idUsuarioDestinatario = dadosDestinatario?.id
+
+        if ( idUsuarioRemetente != null && idUsuarioDestinatario != null ){
+
+            listenerRegistration = firestore
+                .collection(Constantes.MENSAGENS)
+                .document(idUsuarioRemetente)
+                .collection(idUsuarioDestinatario)
+                .orderBy("data", Query.Direction.ASCENDING)
+                .addSnapshotListener { querySnapshot, erro ->
+
+                    if ( erro != null){
+                        exibirMensagem("Erro ao recuperar mensagens")
+                    }
+
+                    val listaMensagens = mutableListOf<Mensagem>()
+                    val documentos = querySnapshot?.documents
+
+                    documentos?.forEach { documentSnapshot ->
+                        val mensagem = documentSnapshot.toObject(Mensagem::class.java)
+                        if (mensagem != null){
+                            listaMensagens.add(mensagem)
+                            Log.i("exibicao_mensagens", mensagem.mensagem)
+                        }
+                    }
+
+                    //Lista
+                    if (listaMensagens.isNotEmpty()){
+                        //Carregar os dados Adapter
+                        conversasAdapter.adicionarLista(listaMensagens)
+                    }
+                }
+
+        }
     }
 
     private fun inicializarEventoClique() {
@@ -64,15 +126,53 @@ class MensagensActivity : AppCompatActivity() {
                     idUsuarioRemetente, idUsuarioDestinatario, mensagem
                 )
 
+                // Foto e nome Destinatario
+
+                val conversaRemetente = Conversa(
+                    idUsuarioRemetente, idUsuarioDestinatario,
+                    dadosDestinatario!!.foto, dadosDestinatario!!.nome,
+                    textoMensagem
+
+                )
+
+                salvarConversaFirestore(conversaRemetente)
+
+
+
 
                 //Salvar mesma mensagem para o destinatário
                 salvarMensagemFirestore(
                     idUsuarioDestinatario, idUsuarioRemetente, mensagem
                 )
 
+                // Foto e nome Remetente
+                val conversaDestinatario = Conversa(
+                    idUsuarioDestinatario, idUsuarioRemetente,
+                    dadosUsuarioRemetente!!.foto, dadosUsuarioRemetente!!.nome,
+                    textoMensagem
+
+
+
+                )
+                salvarConversaFirestore( conversaDestinatario )
+
+                binding.editMensagem.setText("")
+
 
             }
         }
+
+    }
+
+    private fun salvarConversaFirestore(conversa: Conversa) {
+
+
+        firestore.collection(Constantes.CONVERSAS)
+            .document(conversa.idUsuarioRemetente)
+            .collection(Constantes.ULTIMAS_CONVERSAS)
+            .document(conversa.idUsuarioDestinatario)
+            .set(conversa)
+            .addOnSuccessListener { exibirMensagem("Erro ao salvar conversa") }
 
     }
 
@@ -107,8 +207,25 @@ class MensagensActivity : AppCompatActivity() {
         }
     }
 
-    private fun recuperarDadosUsuarioDestinatario() {
+    private fun recuperarDadosUsuarios() {
 
+        //Dados do usuário logado
+        val idUsuarioRemetente = firebaseAuth.currentUser?.uid
+        if ( idUsuarioRemetente != null ){
+            firestore.collection(Constantes.USUARIOS)
+                .document(idUsuarioRemetente)
+                .get()
+                .addOnSuccessListener { documentSnapshot ->
+
+                    val usuario = documentSnapshot.toObject(Usuario::class.java)
+                    if (usuario != null){
+                        dadosUsuarioRemetente = usuario
+                    }
+                }
+        }
+
+
+        //Recuperando dados destinatário
         val extras = intent.extras
         if (extras != null){
             val origem = extras.getString("origem")
